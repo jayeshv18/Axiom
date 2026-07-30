3d5c6d545e081fda202ae398c99073b02158fd39
jayesh v <jayeshv@karunya.edu.in>
2026-07-31 01:12:36 +0530

    refactor(kernel): integrate modular VGA driver API
    - Stripped out the primitive hardcoded pointer logic.
    - Imported the new  header file.
    - Updated  execution loop to dynamically clear the screen and print the initial OS boot sequence using the new rendering engine.

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,6 @@
+#include "include/vga_print.h"
 int main(){
-    char* video_memory = (char*)0xB8000; //pointer to the start of video memory for text mode, further information about this can be found at Theory.txt
-    video_memory[0] = 'X';
-    video_memory[1] = 0x0F; //0x0F is the color code for white text on black background, further information about this can be found at Theory.txt
+    clear_screen();
+    print_string("Hello, World!\n");
     return 0;
 }
